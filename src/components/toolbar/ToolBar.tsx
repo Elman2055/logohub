2e105275ca72b777f1b10439b978846366028453
Elman <200522elmanok>
2024-09-12 18:51:20 +0500

    create: desktop header

--- src/components/toolbar/ToolBar.tsx
+++ src/components/toolbar/ToolBar.tsx
@@ -1,9 +1,53 @@
-import styles from './ToolBar.module.css';
+import { NavLink } from "react-router-dom";
+import search from "../../../public/search.png";
+import favourites from "../../../public/favourites.png";
+import user from "../../../public/user.png";
+import cart from "../../../public/cart.png";
+import styles from "./ToolBar.module.css";
 
 const ToolBar = () => {
   return (
-    <div>ToolBar</div>
-  )
-}
+    <header className={styles.container}>
+      <NavLink to={"/"}>
+        <h2 className={styles.logo}>LOGOHUB</h2>
+      </NavLink>
+      <nav className={styles.navigationContainer}>
+        <NavLink to={"/"}>
+          <p>Каталог</p>
+        </NavLink>
 
-export default ToolBar
+        <NavLink to={"/"}>
+          <p>О нас</p>
+        </NavLink>
+
+        <NavLink to={"/"}>
+          <p>FAQ</p>
+        </NavLink>
+
+        <NavLink to={"/"}>
+          <p>Контакты</p>
+        </NavLink>
+      </nav>
+
+      <div className={styles.imagesContainer}>
+        <NavLink to={"/"}>
+          <img src={search} alt="search" />
+        </NavLink>
+
+        <NavLink to={"/"}>
+          <img src={favourites} alt="favourites" />
+        </NavLink>
+
+        <NavLink to={"/"}>
+          <img src={user} alt="user" />
+        </NavLink>
+
+        <NavLink to={"/"}>
+          <img src={cart} alt="cart" />
+        </NavLink>
+      </div>
+    </header>
+  );
+};
+
+export default ToolBar;
